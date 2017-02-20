884dc448fe59051e586e1c65dcb15809aae70470
David Störmer <noemail@email.de>
2017-02-20 19:29:19 +0100

    Solving the problem. Missing correct index of output.

--- FileFixIt/run.fsx
+++ FileFixIt/run.fsx
@@ -3,7 +3,27 @@ open System.IO;
 open System.Collections.Generic;
 open System.Linq;
 
-let relativePath = @"C:\Gits\FSharpFirstSteps\FileFixIt\tests\sample.txt"
+
+let getAllDirectories (path :string) = 
+    let rec getSubPath (parts : IEnumerable<string>, basePath : string) =
+        if parts.Any()
+            then 
+                let newBasePath = basePath+"/"+parts.First()
+                let remaining = parts.Skip(1)
+                let paths = getSubPath (remaining, newBasePath)
+                seq [basePath] |> Seq.append paths
+            else
+                seq [basePath]
+        
+    let partsOfPath = path.Split ([| '/' |], StringSplitOptions.RemoveEmptyEntries)
+    getSubPath (partsOfPath.Skip(1), "/"+partsOfPath.First())
+
+
+//let relativePath = @"C:\Gits\FSharpFirstSteps\FileFixIt\tests\sample.txt"
+let relativePath = @"X:\FSharpFirstSteps\FileFixIt\tests\sample.txt"
+
+
+
 let lines = 
     File.ReadAllLines relativePath
 
@@ -18,41 +38,54 @@ let rec readTestCasesRec (lines : IEnumerable<string>) : IEnumerable<TestSource>
     let exisiting = lines.Skip(1).Take(numberOfExistingDirectories).ToArray()
     let newDirectories = lines.Skip(1+numberOfExistingDirectories).Take(numberOfNewDirectories).ToArray()
     let testCase =  {ExistingDirectories = exisiting; PotentialNewDirectories = newDirectories;}
+    
     let remainingLines = lines.Skip(1 + numberOfExistingDirectories + numberOfNewDirectories)
     if remainingLines.Any()
         then 
             let testCases = readTestCasesRec remainingLines
-            seq [testCase] |> Seq.append testCases
+            testCases |> Seq.append (seq [testCase])
         else seq [testCase]
 
 
 let readTestCases (lines : string[]) =
     (readTestCasesRec (lines.Skip(1))).ToArray()
 
+
 let solve (test : TestSource) =
     let lookup = List<string>()
-    for path in test.ExistingDirectories do
-        let parts = path.Split('/')
-        for part in parts do
-            if (lookup.Contains(part))
-                then do lookup.Add(part)     
-    let mutable count = 0
-    for path in test.PotentialNewDirectories do
-        let parts = path.Split('/')
-        for part in parts do
-            if(lookup.Contains(part))
-                then 
-                    count = count+1;
-                    do lookup.Add(part)
-
+    let addInitialDirectories (existing:string[]) = 
+        existing
+            |> Seq.map getAllDirectories
+            |> Seq.collect (fun x -> seq { for i in x do yield i})
+            |> Seq.map (fun dir -> 
+                if not (lookup.Contains(dir))
+                        then 
+                            do lookup.Add(dir)
+                            1
+                        else
+                            0)
+            |> Seq.sum
+    let added = addInitialDirectories (test.ExistingDirectories)
+    test.PotentialNewDirectories
+        |> Seq.map getAllDirectories
+        |> Seq.collect (fun x -> seq { for i in x do yield i})
+        |> Seq.map (fun dir -> 
+            if not (lookup.Contains(dir))
+                    then 
+                        do lookup.Add(dir)
+                        1
+                    else
+                        0)
+        |> Seq.sum
     
-    count
-
 let solveAll (tests : IEnumerable<TestSource>) =
     tests
     |> Seq.map solve
-    |> Seq.iter (fun x -> printf "%i" x)
+    |> Seq.map (fun x -> printf "Case #i: %i \r\n" x)
+    |> Seq.iter (fun x -> printf "")
 
 let allTestCases = readTestCases lines
 
 solveAll allTestCases
+
+Console.ReadLine
