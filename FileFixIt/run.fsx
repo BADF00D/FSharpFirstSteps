6899a556abbbed10ddab8b013c7534cd7d325fbd
David Störmer <David.Stoermer@copra-system.de>
2017-02-21 11:53:44 +0100

    Refactoring

--- FileFixIt/run.fsx
+++ FileFixIt/run.fsx
@@ -3,6 +3,7 @@ open System.IO;
 open System.Collections.Generic;
 open System.Linq;
 
+let split (ch : char) (s : string) = s.Split(ch)
 
 let getAllDirectories (path :string) = 
     let rec getSubPath (parts : IEnumerable<string>, basePath : string) =
@@ -19,7 +20,8 @@ let getAllDirectories (path :string) =
     getSubPath (partsOfPath.Skip(1), "/"+partsOfPath.First())
 
 
-let pathToTests = @"C:\Gits\FSharpFirstSteps\FileFixIt\tests\"
+//let pathToTests = @"C:\Gits\FSharpFirstSteps\FileFixIt\tests\"
+let pathToTests = @"C:\Git\FSharpFirstSteps\FileFixIt\tests"
 let currentFile = @"A-large-practice.in"
 //let pathToTests = @"X:\FSharpFirstSteps\FileFixIt\tests\sample.txt"
 
@@ -31,38 +33,50 @@ let lines =
 
 let numberOfTests = lines.[0]
 
-type TestSource  = { ExistingDirectories : string[] ; PotentialNewDirectories : string[]}
+type TestSource  = { ExistingDirectories : string list ; PotentialNewDirectories : string list}
 
-let rec readTestCasesRec (lines : IEnumerable<string>) : IEnumerable<TestSource> =
-    let header = lines.First().Split(' ');
-    let numberOfExistingDirectories = Int32.Parse(header.[0])
-    let numberOfNewDirectories = Int32.Parse(header.[1])
-    let exisiting = lines.Skip(1).Take(numberOfExistingDirectories).ToArray()
-    let newDirectories = lines.Skip(1+numberOfExistingDirectories).Take(numberOfNewDirectories).ToArray()
-    let testCase =  {ExistingDirectories = exisiting; PotentialNewDirectories = newDirectories;}
-    
-    let remainingLines = lines.Skip(1 + numberOfExistingDirectories + numberOfNewDirectories)
-    if remainingLines.Any()
-        then 
-            let testCases = readTestCasesRec remainingLines
-            testCases |> Seq.append (seq [testCase])
-        else seq [testCase]
+let rec readTestCasesRec (acc : TestSource list) (lines : string list) : TestSource list =
+    match lines with
+    | [] -> acc
+    | _ ->
+        let [numExisting; numNew] = 
+            lines 
+            |> Seq.head 
+            |> split ' ' 
+            |> List.ofArray
+            |> List.map int
+        
+        let (exisitingDirs, newDirs, rest) = 
+            lines
+            |> List.skip 1
+            |> List.splitAt numExisting
+            |> fun (ex, re) -> (ex, re |> List.splitAt numNew)
+            |> fun (ex, (ne, re)) -> (ex, ne, re)
 
+        let newDirectories = lines.Skip(1+numExisting).Take(numNew).ToArray()
+        let testCase =  { ExistingDirectories = exisitingDirs; PotentialNewDirectories = newDirs; }
+        
+        readTestCasesRec (testCase :: acc) rest
 
 let readTestCases (lines : string[]) =
-    (readTestCasesRec (lines.Skip(1))).ToArray()
+    lines 
+    |> List.ofSeq
+    |> List.skip 1
+    |> readTestCasesRec []
+    |> List.rev
+    //(readTestCasesRec (lines.Skip(1))).ToArray()
 
 
 let solve (test : TestSource) =
     let lookup = List<string>()
-    let addInitialDirectories (existing:string[]) = 
+    let addInitialDirectories (existing:string list) = 
         existing
             |> Seq.map getAllDirectories
             |> Seq.collect (fun x -> seq { for i in x do yield i})
             |> Seq.map (fun dir -> 
-                if not (lookup.Contains(dir))
+                if not (lookup.Contains dir)
                         then 
-                            do lookup.Add(dir)
+                            do lookup.Add dir
                             1
                         else
                             0)
@@ -80,20 +94,24 @@ let solve (test : TestSource) =
                         0)
         |> Seq.sum
     
-let solveAll (tests : IEnumerable<TestSource>) =
+let solveAll (tests : TestSource seq) =
     let fullPath = Path.Combine(pathToTests, currentFile+".result.txt")
-    use writer = new StreamWriter(new FileStream(fullPath, FileMode.Create))
+    use stream = new FileStream(fullPath, FileMode.Create)
+    use writer = new StreamWriter(stream)
     tests
     |> Seq.map solve
     |> Seq.iteri (fun i x -> 
-        let result = "Case #"+(i+1).ToString()+": "+x.ToString()
-        writer.WriteLine result
-        ignore writer.Flush 
+        //let result = "Case #"+(i+1).ToString()+": "+x.ToString()
+        // writer.WriteLine result
+        sprintf "Case #%i: %i" (i + 1) x
+        |> writer.WriteLine
+        //ignore writer.Flush 
     )
+    writer.Flush |> ignore
     
 
 let allTestCases = readTestCases lines
 
 solveAll allTestCases
 
-Console.ReadLine
+Console.ReadLine ()
